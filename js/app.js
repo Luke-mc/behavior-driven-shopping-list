0f096ecf5befa0b95daf1cd054bcef0a15917aa9
jeffmon <jeffmon@gmail.com>
2017-06-15 21:52:27 -1000

    started index.html

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+var targetButton = document.getElementById("add_shopping_list_item_button");
+var targetTitle = document.getElementById("title");
+var targetDescription = document.getElementById("description");
+var targetContent = document.getElementById("content");
+
+var newList = new ShoppingList();
+
+
+var new_shopping_list_item;
+
+function add_to_shopping_list(){
+  new_shopping_list_item = new ShoppingListItem(targetTitle.value, targetDescription.value);
+  newList.addItem(new_shopping_list_item);
+  targetContent.innerHTML = newList.render();
+}
+
+targetButton.addEventListener("click", add_to_shopping_list);
