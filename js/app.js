035a936a647d7ac03b3f161d92c94b4d94b32730
jeffmon <jeffmon@gmail.com>
2017-06-17 16:50:57 -1000

    added checkbox changes

--- js/app.js
+++ js/app.js
@@ -15,10 +15,27 @@ function add_to_shopping_list(){
   new_shopping_list_item = new ShoppingListItem(targetTitle.value, targetDescription.value);
   newList.addItem(new_shopping_list_item);
   targetContent.innerHTML = newList.render();
-
+/*  check[0].onchange = function(){
+    if(this.checked){
+      console.log("checked");
+    } else{
+      console.log("not checked");
+    }
+  };*/
+/*  check[0].addEventListener("change", function(){
+    console.log("hello");
+  });*/
+  for(var i = 0; i<check.length; i++){
+    check[i].addEventListener("change", function(){
+      if(this.checked === true){
+        console.log(this);
+      } else if(this.checked === false){
+        console.log("unchecked!");
+      }
+    });
+  }
 }
 
 targetButton.addEventListener("click", add_to_shopping_list);
-
 
 
